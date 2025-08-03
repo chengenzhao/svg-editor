b13e41ce761dce04dddf9c400fb324f92a6a90f2
chengenzhao <chengen.zhao@mail.mcgill.ca>
2025-08-03 15:38:13 +0800

    1st commit

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,5 @@ module com.whitewoodcity.svgeditor {
     requires javafx.controls;
     requires com.almasb.fxgl.all;
 
-    opens com.whitewoodcity.svgeditor to javafx.fxml;
     exports com.whitewoodcity.svgeditor;
 }
